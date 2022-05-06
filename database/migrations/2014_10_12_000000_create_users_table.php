d0235647a61ee9b4fbf8e388ccf455d89b00ee11
Davide <davide.deacetis13@gmail.com>
2022-05-06 16:47:16 +0200

    Utente: creato il modello e la migrazione per l'entità utente.
    ToDo: creare le tabelle per le specializzazioni di ongi utente (Manager, Ricercatore, Finanziatore)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nome');
+            $table->string('cognome');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('ruolo');
             $table->rememberToken();
             $table->timestamps();
         });

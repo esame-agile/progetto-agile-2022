fefc1f5ebe1ad68adb366f0d4760ad5a6d3d9ef6
Davide <davide.deacetis13@gmail.com>
2022-05-10 11:20:09 +0200

    Merge branch di Domenico

--- app/Http/Controllers/Auth/RegisteredUtenteController.php
+++ app/Http/Controllers/Auth/RegisteredUtenteController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\Utente;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 
-class RegisteredUserController extends Controller
+class RegisteredUtenteController extends Controller
 {
     /**
      * Display the registration view.
@@ -34,13 +34,13 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'nome' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
+        $user = Utente::create([
+            'nome' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);

0074bdb0b3c4c336e1e5536b0344279850938ab3
Davide <davide.deacetis13@gmail.com>
2022-05-12 20:43:56 +0200

    Creata pagina personale del ricercatore.

---
+++ app/Http/Controllers/PaginaPersonaleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PaginaPersonaleController extends Controller
+{
+    public function index()
+    {
+        $nav = [
+            ['label' => 'HOME', 'class' => 'nav-link', 'href' => 'http://127.0.0.1:8000/'],
+            ['label' => 'LOG OUT', 'class' => 'nav-link', 'href' => 'http://127.0.0.1:8000/login']
+        ];
+
+        return view('pagina-personale', compact('nav'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,31 @@
 <?php
 
+use App\Http\Controllers\PaginaPersonaleController;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Vista home.
+ *
+ */
 Route::get('/', function () {
     $nav = [
         ['label' => 'CHI SIAMO', 'class' => 'page-scroll', 'href' => '#service'],
         ['label' => 'TOP 5', 'class' => 'page-scroll', 'href' => '#testimonial'],
         ['label' => 'LOG IN', 'class' => 'nav-link', 'href' => 'http://127.0.0.1:8000/login']
     ];
-
     return view('home', compact('nav'));
 });
 
+/**
+ * Vista pagina personale di un ricercatore.
+ *
+ */
+Route::get('/pagina-personale', [PaginaPersonaleController::class, 'index'])->name('pagina-personale');
+
+/**
+ * Vista login.
+ *
+ */
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');

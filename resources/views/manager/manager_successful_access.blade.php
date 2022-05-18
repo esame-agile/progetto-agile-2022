f4413bf9b96b3b3f8a5fbae0f6a30ba5a1c34cd6
pameladiclemente <pamela.diclemente@student.univaq.it>
2022-05-18 18:23:26 +0200

    Modifiche login (push per pair programming)

---
+++ resources/views/manager/manager_successful_access.blade.php
@@ -0,0 +1,16 @@
+@extends("layouts/main")
+
+@section('content')
+
+
+
+<div class="card"> voglio morire
+
+    <p> erfe</p>
+    <p> erfe</p>
+    <p> erfe</p>
+    <p> erfe</p>
+
+</div>
+
+@endsection

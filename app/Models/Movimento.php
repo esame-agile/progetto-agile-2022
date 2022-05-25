8ed28acdcdbd069b8d70694af7c6088f841eb425
ralucaM-Bujoreanu <raluca.lalu@hotmail.com>
2022-05-25 11:10:43 +0200

    modelli

---
+++ app/Models/Movimento.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Movimento extends Model
+{
+    use HasApiTokens, HasFactory, Notifiable;
+    protected $table = 'movimenti';
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'importo',
+        'causale',
+        'data',
+        'budget_id'
+    ];
+
+    public function budget(){
+        return $this->belongsTo(Budget::class, "budget_id");
+    }
+
+
+}
---
+++ app/Models/Report.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Report extends Model
+{
+    use HasApiTokens, HasFactory, Notifiable;
+    protected $table = 'reports';
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'titolo',
+        'file_name',
+        'data',
+        'autore_id',
+
+    ];
+
+    public function autore(){
+        return $this->belongsTo(Ricercatore::class, 'autore_id');
+    }
+
+    public function sotto_progetto(){
+        return $this->belongsTo(SottoProgetto::class);
+    }
+    public function progetto(){
+        return $this->belongsTo(Progetto::class);
+    }
+}
